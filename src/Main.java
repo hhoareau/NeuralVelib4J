1f318f00b2ca9ee1cc3587a080f720d2d9333127
hhoareau <hhoareau@gmail.com>
2017-02-21 12:58:31 +0100

    Commit

--- src/Main.java
+++ src/Main.java
@@ -9,71 +9,94 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import spark.Spark;
 
-import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
 
     static SparkSession spark=null;
 
-
-    public static void init(Dataset<Row> stations){
-
-        stations.show(20,false);
-
+    public static void train(MultilayerPerceptronClassifier trainer,Dataset<Row> stations){
         VectorAssembler assembler = new VectorAssembler()
-                .setInputCols(new String[]{"day", "hour","month","minute","temperature"})
+                .setInputCols(new String[]{"id","lt","lg","day", "hour","month","minute","temperature"})
                 .setOutputCol("features");
-
         Dataset<Row> rows=assembler.transform(stations);
-        rows=rows.drop(new String[]{"id","day", "hour", "lg","lt","month","minute","temperature"});
-        rows.show(20,false);
+        Dataset<Row>[] splits = rows.randomSplit(new double[]{0.6, 0.4},1234L);
 
-        Dataset<Row>[] splits = rows.randomSplit(new double[]{0.6, 0.4}, 1234L);
-        Dataset<Row> train = splits[0];
-        Dataset<Row> test = splits[1];
-
-// specify layers for the neural network:
-// input layer of size 4 (features), two intermediate of size 5 and 4
-// and output of size 3 (classes)
-        int[] layers = new int[] {6, 5, 4, 3};
-
-// create the trainer and set its parameters
-        MultilayerPerceptronClassifier trainer = new MultilayerPerceptronClassifier()
-                .setLayers(layers)
-                .setBlockSize(128)
-                .setSeed(1234L)
-                .setLabelCol("nPlace")
-                .setMaxIter(100);
-
-// train the model
-        MultilayerPerceptronClassificationModel model = trainer.fit(train);
-
-// compute accuracy on the test set
-        Dataset<Row> result = model.transform(test);
-        Dataset<Row> predictionAndLabels = result.select("prediction", "label");
-        MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator()
-                .setMetricName("accuracy");
-
+        MultilayerPerceptronClassificationModel model = trainer.fit(rows);
     }
+
+
+    public static double evaluate(MultilayerPerceptronClassificationModel model, Dataset<Row> test){
+        Dataset<Row> result = model.transform(test);
+        Dataset<Row> predictionAndLabels = result.select("prediction", "nPlace");
+        MulticlassClassificationEvaluator evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy").setLabelCol("nPlace");
+        return evaluator.evaluate(predictionAndLabels);
+    }
+
+
+    /**
+     *
+     * @param str
+     * @return
+     * @throws IOException
+     */
+    public static List<String[]> getCSV(String str) throws IOException {
+        List<String[]> rc=new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(str));
+        String line = null;
+
+        String[] col=null;
+
+        int i=0;
+        while ((line = br.readLine()) != null) {
+            if(i==0)col=line.split(";");
+            String[] values = line.split(";");
+            rc.add(values);
+            i++;
+        }
+        br.close();
+        return rc;
+    }
+
+
+    //https://donneespubliques.meteofrance.fr/?fond=donnee_libre&prefixe=Txt%2FSynop%2Fsynop&extension=csv&date=20170221&reseau=09
+    public static Map<String,Double> getMeteo(Date dt) throws IOException {
+        Map<String,Double> rc=new HashMap<>();
+
+        String sDate=new SimpleDateFormat("yyyyMMdd").format(dt);
+        String path="synop."+sDate+"09.csv";
+        String url="https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/synop."+sDate+"09.csv";
+        File f=new File(path);
+        if(!f.exists()){
+            Response r=ClientBuilder.newClient().target(url).request(MediaType.TEXT_PLAIN).get();
+            FileOutputStream fo=new FileOutputStream(f);
+            fo.write(r.readEntity(String.class).getBytes());
+            fo.close();
+        }
+        for(String[] s:getCSV(path))
+            if(s[0].equals("07190")){
+                rc.put("temperature",Double.valueOf(s[7])-270);
+                rc.put("uv",Double.valueOf(s[9]));
+                break;
+            }
+        return rc;
+    }
+
 
     public static JsonNode getData(String str)  {
         try {
             if(str.startsWith("http")){
-                Client client = ClientBuilder.newClient();
-                WebTarget target = client.target(str);
-                Response r=target.request(MediaType.APPLICATION_JSON).get();
+                Response r=ClientBuilder.newClient().target(str).request(MediaType.APPLICATION_JSON).get();
                 Integer code=r.getStatus();
                 if(code==200){
                     return new ObjectMapper().readTree(new InputStreamReader(r.readEntity(InputStream.class)));
@@ -89,21 +112,57 @@ public class Main {
         return null;
     }
 
-    public static Dataset<Row> createTrain(JsonNode jsonNode){
+
+    public static Dataset<Row> createTrain(String file){
+        return spark.read().json(file);
+    }
+
+
+    public static Dataset<Row> createTrain(JsonNode jsonNode) throws IOException {
         List<Station> stations= new ArrayList<>();
 
         Iterator<JsonNode> ite=jsonNode.getElements();
-        while(ite.hasNext())
-            stations.add(new Station(ite.next().get("fields"),10.0, System.currentTimeMillis()));
+        Double temperature=0.0;
+        int uv=0;
+        Map<String,Double> data=getMeteo(new Date(System.currentTimeMillis()));
 
-        return spark.createDataFrame(stations,Station.class);
+        while(ite.hasNext())
+            stations.add(new Station(ite.next().get("fields"), data.get("temperature"), System.currentTimeMillis()));
+
+        Dataset<Row> rc=spark.createDataFrame(stations,Station.class);
+        return rc.persist();
     }
 
-    public static void main(String[] args) {
-        Spark.port(8080);
-        spark= SparkSession.builder().master("local").appName("Java Spark SQL basic example").getOrCreate();
 
-        init(createTrain(getData("stations-velib-disponibilites-en-temps-reel.json")));
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    public static MultilayerPerceptronClassifier trainer=null;
+
+    public static void main(String[] args) throws IOException {
+        Spark.port(8080);
+
+        // create the trainer and set its parameters
+        trainer = new MultilayerPerceptronClassifier()
+                .setLayers(new int[] {8, 5, 4, 3})
+                .setBlockSize(128)
+                .setSeed(1234L)
+                .setLabelCol("nPlace")
+                .setMaxIter(1000);
+
+
+        final Runnable commandRefresh = new Runnable() {
+            public void run() {
+                try {
+                    train(trainer,createTrain(getData("stations-velib-disponibilites-en-temps-reel.json")));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                scheduler.schedule(this,5, TimeUnit.MINUTES);
+            }
+        };
+        scheduler.schedule(commandRefresh,0,TimeUnit.MINUTES);
+
+
+        spark= SparkSession.builder().master("local").appName("Java Spark SQL basic example").getOrCreate();
 
         Spark.get("/getdata", (request, response) -> {
             return "ok";
--- src/Station.java
+++ src/Station.java
@@ -1,42 +1,57 @@
-import org.apache.spark.ml.linalg.Vectors;
-import org.apache.spark.sql.RowFactory;
 import org.codehaus.jackson.JsonNode;
-
-import java.io.Serializable;
+import scala.Serializable;
 import java.util.Date;
 
 /**
  * Created by Hervé on 20/02/2017.
  */
 public class Station implements Serializable {
+    Long id=0L;
+
     Double lt=0.0;
     Double lg=0.0;
     Double temperature=0.0;
-    Long id=null;
     Double month=0.0;
     Double minute=0.0;
     Double day=0.0;
     Double hour=0.0;
-    Integer nPlace=0; //0,1 ou 2
+    Double nPlace=0.0; //0,1 ou 2
 
     public Station(JsonNode jnode, Double temperature, Long dt){
-        this.nPlace=jnode.get("available_bike_stands").asInt();
-        if(this.nPlace>0 && this.nPlace<10)this.nPlace=1;
-        if(this.nPlace>=10)this.nPlace=2;
+        this.id=jnode.get("number").asLong();
+
+        this.nPlace=jnode.get("available_bike_stands").asDouble();
+        if(this.nPlace>0 && this.nPlace<10)this.nPlace=1.0;
+        if(this.nPlace>=10)this.nPlace=2.0;
 
         this.lt= Double.valueOf(jnode.get("position").get(0).asDouble());
         this.lg= Double.valueOf(jnode.get("position").get(1).asDouble());
-        this.id=jnode.get("number").asLong();
+
         this.temperature=temperature/30.0;
         this.day=new Date(dt).getDay()/7.0;
         this.minute=new Date(dt).getMinutes()/60.0;
         this.month=new Date(dt).getMonth()/12.0;
         this.hour=new Date(dt).getHours()/24.0;
-        //Row rc= RowFactory.create(nPlace, Vectors.dense(id, position, temperature,0.1,new Date(dt).getHours(),,new Date().getMinutes()));
+
+        //this.features=new SparseVector(6,new int[]{0,1,2,3,4,5},new double[]{this.day,this.minute,this.month,this.day,this.temperature,this.id});
     }
 
+    /*
     public org.apache.spark.sql.Row toRow(){
         return RowFactory.create(this.nPlace, Vectors.dense(id, this.lt,this.lg, this.temperature,0.1,this.day,this.month,this.hour,this.minute));
+    }
+    */
+
+    //public SparseVector getFeatures() {return features;}
+    //public void setFeatures(SparseVector features) {this.features = features;}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Double getLt() {
@@ -61,14 +76,6 @@ public class Station implements Serializable {
 
     public void setTemperature(Double temperature) {
         this.temperature = temperature;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public Double getMonth() {
@@ -103,11 +110,11 @@ public class Station implements Serializable {
         this.hour = hour;
     }
 
-    public Integer getnPlace() {
+    public Double getnPlace() {
         return nPlace;
     }
 
-    public void setnPlace(Integer nPlace) {
+    public void setnPlace(Double nPlace) {
         this.nPlace = nPlace;
     }
 }
